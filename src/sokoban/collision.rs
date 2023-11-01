c0e4657a70b8f4077cd97c570b0cc96875b26a01
Syniis <fynn.c.lohren@gmail.com>
2023-11-01 15:48:30 +0100

    Seperated collision logic into own module

---
+++ src/sokoban/collision.rs
@@ -0,0 +1,105 @@
+use bevy::{log, prelude::*};
+use bevy_ecs_tilemap::{prelude::TilemapSize, tiles::TileStorage};
+use bevy_pile::grid::Grid;
+
+use super::{Dir, GameState, Pos, SokobanBlock};
+
+pub struct CollisionPlugin;
+
+impl Plugin for CollisionPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_type::<CollisionMap>()
+            .add_systems(
+                Update,
+                (init_collision_map).run_if(in_state(GameState::Play)),
+            )
+            .add_systems(
+                PostUpdate,
+                sync_collision_map.run_if(in_state(GameState::Play)),
+            );
+    }
+}
+
+#[derive(Resource, Reflect)]
+#[reflect(Resource)]
+pub struct CollisionMap {
+    map: Grid<Option<(Entity, SokobanBlock)>>,
+}
+
+impl Default for CollisionMap {
+    fn default() -> Self {
+        Self {
+            map: Grid::new(IVec2::new(0, 0), None),
+        }
+    }
+}
+
+fn init_collision_map(
+    mut cmds: Commands,
+    tilemap: Query<&TilemapSize, Added<TileStorage>>,
+    sokoban_entities: Query<(Entity, &Pos, &SokobanBlock)>,
+) {
+    let Some(size) = tilemap.get_single().ok() else {
+        return;
+    };
+    log::info!("Initialized collision map");
+    let mut map = Grid::new(IVec2::new(size.x as i32, size.y as i32), None);
+    for (entity, pos, block) in sokoban_entities.iter() {
+        let pos = IVec2::from(pos);
+        map.set(pos, Some((entity, *block)));
+    }
+    cmds.insert_resource(CollisionMap { map });
+}
+
+// TODO dont rebuild but instead only change moved entities
+fn sync_collision_map(
+    mut collision: ResMut<CollisionMap>,
+    sokoban_entities: Query<(Entity, &Pos, &SokobanBlock)>,
+) {
+    collision.map.iter_mut().for_each(|(_, elem)| {
+        elem.take();
+    });
+    for (entity, pos, block) in sokoban_entities.iter() {
+        collision.map.set(IVec2::from(pos), Some((entity, *block)));
+    }
+}
+
+pub enum CollisionResult {
+    Push(Vec<Entity>),
+    Wall,
+    OutOfBounds,
+}
+
+impl CollisionMap {
+    pub fn push_collision(&self, pusher_pos: IVec2, direction: Dir) -> CollisionResult {
+        let Some(Some((pusher, _))) = self.map.get(pusher_pos) else {
+            return CollisionResult::OutOfBounds;
+        };
+
+        let move_in_dir = |pos| -> IVec2 { pos + IVec2::from(direction) };
+        let mut moving_entities = Vec::new();
+        let mut current_pos = pusher_pos;
+        let mut dest = move_in_dir(current_pos);
+        let mut pusher = pusher;
+        while let Some(dest_entity) = self.map.get(dest) {
+            match dest_entity {
+                Some((pushed, block)) => match block {
+                    SokobanBlock::Static => {
+                        return CollisionResult::Wall;
+                    }
+                    SokobanBlock::Dynamic => {
+                        moving_entities.push(*pusher);
+                        pusher = pushed;
+                        current_pos = dest;
+                        dest = move_in_dir(current_pos);
+                    }
+                },
+                None => {
+                    moving_entities.push(*pusher);
+                    break;
+                }
+            }
+        }
+        CollisionResult::Push(moving_entities)
+    }
+}
--- src/sokoban/mod.rs
+++ src/sokoban/mod.rs
@@ -1,20 +1,19 @@
 use bevy::{ecs::system::SystemParam, log, prelude::*};
-use bevy_ecs_tilemap::{
-    prelude::TilemapSize,
-    tiles::{TilePos, TileStorage},
-};
-use bevy_pile::{grid::Grid, tilemap::tile_to_world_pos};
+use bevy_ecs_tilemap::tiles::TilePos;
+use bevy_pile::tilemap::tile_to_world_pos;
 use leafwing_input_manager::prelude::*;
 
 use crate::sokoban::momentum::Momentum;
 
 use self::{
+    collision::{CollisionMap, CollisionPlugin, CollisionResult},
     goal::GoalPlugin,
     history::{HandleHistoryEvents, History, HistoryEvent, HistoryPlugin},
     momentum::MomentumPlugin,
     player::PlayerPlugin,
 };
 
+pub mod collision;
 pub mod cube;
 pub mod goal;
 pub mod history;
@@ -31,15 +30,15 @@ impl Plugin for SokobanPlugin {
             InputManagerPlugin::<SokobanActions>::default(),
             MomentumPlugin,
             GoalPlugin,
+            CollisionPlugin,
         ))
         .add_state::<GameState>()
         .register_type::<Pos>()
         .register_type::<Dir>()
         .register_type::<History<Pos>>()
         .register_type::<SokobanBlock>()
-        .register_type::<CollisionMap>()
         .add_event::<SokobanEvent>()
-        .add_systems(Startup, (setup, apply_deferred, init_collision_map).chain())
+        .add_systems(Startup, setup)
         .add_systems(
             Update,
             (
@@ -55,7 +54,7 @@ impl Plugin for SokobanPlugin {
                 (play).run_if(in_state(GameState::LevelSelect)),
             ),
         )
-        .add_systems(PostUpdate, (copy_pos_to_transform, sync_collision_map));
+        .add_systems(PostUpdate, copy_pos_to_transform);
     }
 }
 
@@ -277,88 +276,4 @@ pub struct PushEvent {
     pub pusher: Entity,
     pub direction: Dir,
     pub pushed: Vec<Entity>,
-}
-
-#[derive(Resource, Reflect)]
-#[reflect(Resource)]
-pub struct CollisionMap {
-    map: Grid<Option<(Entity, SokobanBlock)>>,
-}
-
-impl Default for CollisionMap {
-    fn default() -> Self {
-        Self {
-            map: Grid::new(IVec2::new(0, 0), None),
-        }
-    }
-}
-
-fn init_collision_map(
-    mut cmds: Commands,
-    tilemap: Query<&TilemapSize, Added<TileStorage>>,
-    sokoban_entities: Query<(Entity, &Pos, &SokobanBlock)>,
-) {
-    let Some(size) = tilemap.get_single().ok() else {
-        return;
-    };
-    log::info!("Initialized collision map");
-    let mut map = Grid::new(IVec2::new(size.x as i32, size.y as i32), None);
-    for (entity, pos, block) in sokoban_entities.iter() {
-        let pos = IVec2::from(pos);
-        map.set(pos, Some((entity, *block)));
-    }
-    cmds.insert_resource(CollisionMap { map });
-}
-
-// TODO dont rebuild but instead only change moved entities
-fn sync_collision_map(
-    mut collision: ResMut<CollisionMap>,
-    sokoban_entities: Query<(Entity, &Pos, &SokobanBlock)>,
-) {
-    collision.map.iter_mut().for_each(|(_, elem)| {
-        elem.take();
-    });
-    for (entity, pos, block) in sokoban_entities.iter() {
-        collision.map.set(IVec2::from(pos), Some((entity, *block)));
-    }
-}
-
-enum CollisionResult {
-    Push(Vec<Entity>),
-    Wall,
-    OutOfBounds,
-}
-
-impl CollisionMap {
-    fn push_collision(&self, pusher_pos: IVec2, direction: Dir) -> CollisionResult {
-        let Some(Some((pusher, _))) = self.map.get(pusher_pos) else {
-            return CollisionResult::OutOfBounds;
-        };
-
-        let move_in_dir = |pos| -> IVec2 { pos + IVec2::from(direction) };
-        let mut moving_entities = Vec::new();
-        let mut current_pos = pusher_pos;
-        let mut dest = move_in_dir(current_pos);
-        let mut pusher = pusher;
-        while let Some(dest_entity) = self.map.get(dest) {
-            match dest_entity {
-                Some((pushed, block)) => match block {
-                    SokobanBlock::Static => {
-                        return CollisionResult::Wall;
-                    }
-                    SokobanBlock::Dynamic => {
-                        moving_entities.push(*pusher);
-                        pusher = pushed;
-                        current_pos = dest;
-                        dest = move_in_dir(current_pos);
-                    }
-                },
-                None => {
-                    moving_entities.push(*pusher);
-                    break;
-                }
-            }
-        }
-        CollisionResult::Push(moving_entities)
-    }
 }
